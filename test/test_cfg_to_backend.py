3b4b2737bc0e04720e4ca66e39291ae4a4bd67ce
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-07-18 10:34:01 +0200

    Few code cleaning in tests modules

--- test/test_cfg_to_backend.py
+++ test/test_cfg_to_backend.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
 import unittest2
+import os
 import time
 import json
 import subprocess
@@ -9,13 +12,17 @@ from alignak_backend_client.client import Backend
 
 
 class TestCfgToBackend(unittest2.TestCase):
-
     @classmethod
     def setUpClass(cls):
-        cls.maxDiff=None
-        # cls.p = subprocess.Popen(['uwsgi', '-w', 'alignakbackend:app', '--socket', '0.0.0.0:5000', '--protocol=http', '--enable-threads'])
+        cls.maxDiff = None
+        # cls.p = subprocess.Popen(['uwsgi', '-w', 'alignakbackend:app', '--socket',
+        # '0.0.0.0:5000', '--protocol=http', '--enable-threads'])
+        # Set test mode for alignak backend
+        os.environ['TEST_ALIGNAK_BACKEND'] = '1'
+        os.environ['ALIGNAK_BACKEND_MONGO_DBNAME'] = 'alignak-backend-import-test'
+
         cls.p = subprocess.Popen(['alignak_backend'])
-        print ("Backend PID: %s" % cls.p)
+        print("Backend PID: %s" % cls.p)
         time.sleep(3)
 
         cls.backend = Backend('http://127.0.0.1:5000')
@@ -42,8 +49,9 @@ class TestCfgToBackend(unittest2.TestCase):
         cls.backend.delete("livesynthesis", {})
 
     def test_timeperiod(self):
-        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete', 'alignak_cfg_files/timeperiods.cfg'])
-        (stdoutdata, stderrdata) = q.communicate() # now wait
+        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete',
+                              'alignak_cfg_files/timeperiods.cfg'])
+        q.communicate()  # now wait
 
         result = self.backend.get('timeperiod')
         tps = result['_items']
@@ -54,7 +62,8 @@ class TestCfgToBackend(unittest2.TestCase):
                    u"notes": u"",
                    u"alias": u"Normal Work Hours",
                    u"dateranges": [{u'monday': u'09:00-17:00'}, {u'tuesday': u'09:00-17:00'},
-                                   {u'friday': u'09:00-12:00,14:00-16:00'}, {u'wednesday': u'09:00-17:00'},
+                                   {u'friday': u'09:00-12:00,14:00-16:00'},
+                                   {u'wednesday': u'09:00-17:00'},
                                    {u'thursday': u'09:00-17:00'}],
                    u"exclude": [], u"is_active": False, u"imported_from": u"alignak_backend_import"
                    }
@@ -67,8 +76,9 @@ class TestCfgToBackend(unittest2.TestCase):
             self.assertEqual(comm, ref)
 
     def test_timeperiod_complex(self):
-        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete', 'alignak_cfg_files/timeperiods_complex.cfg'])
-        (_, _) = q.communicate() # now wait
+        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete',
+                              'alignak_cfg_files/timeperiods_complex.cfg'])
+        (_, _) = q.communicate()  # now wait
 
         r = self.backend.get_all('timeperiod')
         r = r['_items']
@@ -78,7 +88,8 @@ class TestCfgToBackend(unittest2.TestCase):
                u"alias": u"Normal Work Hours",
                u"notes": u"",
                u"dateranges": [{u'monday': u'09:00-17:00'}, {u'tuesday': u'09:00-17:00'},
-                               {u'friday': u'09:00-12:00,14:00-16:00'}, {u'wednesday': u'09:00-17:00'},
+                               {u'friday': u'09:00-12:00,14:00-16:00'},
+                               {u'wednesday': u'09:00-17:00'},
                                {u'thursday': u'09:00-17:00'}],
                u"exclude": [u'us-holidays'], u"is_active": False,
                u"imported_from": u"alignak_backend_import"}
@@ -117,33 +128,35 @@ class TestCfgToBackend(unittest2.TestCase):
         :return: None
         """
         # host.hostgroups
-        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete', 'alignak_cfg_files/hosts_links_hostgroup.cfg'])
-        (stdoutdata, stderrdata) = q.communicate() # now wait
+        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete',
+                              'alignak_cfg_files/hosts_links_hostgroup.cfg'])
+        q.communicate()  # now wait
 
         result = self.backend.get('host')
         hosts = result['_items']
         self.assertEqual(len(hosts), 1)
         for host in hosts:
             # host_id = hosts[0]['_id']
-            print "Host:", host
+            print("Host:", host)
 
         result = self.backend.get('hostgroup')
         hostgroups = result['_items']
         self.assertEqual(len(hostgroups), 3)
         for hostgroup in hostgroups:
-            print "Hostgroup:", hostgroup
-            print "Hostgroup groups:", hostgroup['hostgroups']
+            print("Hostgroup:", hostgroup)
+            print("Hostgroup groups:", hostgroup['hostgroups'])
             # self.assertEqual(hostgroup['hosts'], [hosts[0]['_id']])
 
     def test_host_multiple_link_later(self):
-        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete', 'alignak_cfg_files/hosts_links_parent.cfg'])
-        (stdoutdata, stderrdata) = q.communicate() # now wait
+        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete',
+                              'alignak_cfg_files/hosts_links_parent.cfg'])
+        q.communicate()  # now wait
 
         result = self.backend.get('host')
         hosts = result['_items']
         self.assertEqual(len(hosts), 3)
         for host in hosts:
-            print "Host:", host['name']
+            print("Host:", host['name'])
             if host['name'] == 'webui':
                 webui = host.copy()
             if host['name'] == 'backend':
@@ -151,19 +164,20 @@ class TestCfgToBackend(unittest2.TestCase):
             if host['name'] == 'mongo':
                 mongo = host.copy()
 
-        print backend['parents']
+        print(backend['parents'])
         self.assertEqual(backend['parents'], [])
-        print mongo['parents']
+        print(mongo['parents'])
         self.assertEqual(mongo['parents'], [backend['_id']])
-        print webui['parents']
+        print(webui['parents'])
         self.assertEqual(webui['parents'], [backend['_id'], mongo['_id']])
 
     def test_hostgroups_links(self):
         """
         """
         # host.hostgroups
-        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete', 'alignak_cfg_files/hostgroups_links_hostgroup.cfg'])
-        (stdoutdata, stderrdata) = q.communicate() # now wait
+        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete',
+                              'alignak_cfg_files/hostgroups_links_hostgroup.cfg'])
+        q.communicate()  # now wait
 
         result = self.backend.get('host')
         hosts = result['_items']
@@ -174,17 +188,18 @@ class TestCfgToBackend(unittest2.TestCase):
         hostgroups = result['_items']
         self.assertEqual(len(hostgroups), 3)
         for hostgroup in hostgroups:
-            print "Hostgroup:", hostgroup
-            print "Hostgroup groups members:", hostgroup['hostgroups']
-            print "Hostgroup members:", hostgroup['hosts']
+            print("Hostgroup:", hostgroup)
+            print("Hostgroup groups members:", hostgroup['hostgroups'])
+            print("Hostgroup members:", hostgroup['hosts'])
             if hostgroup['name'] == 'freebsd':
                 self.assertEqual(len(hostgroup['hostgroups']), 1)
             if hostgroup['name'] == 'alignak':
                 self.assertEqual(len(hostgroup['hostgroups']), 0)
 
     def test_command_with_args(self):
-        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete', 'alignak_cfg_files/hosts.cfg'])
-        (stdoutdata, stderrdata) = q.communicate() # now wait
+        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete',
+                              'alignak_cfg_files/hosts.cfg'])
+        q.communicate()  # now wait
 
         c = self.backend.get('command')
         self.assertEqual(len(c['_items']), 1)
@@ -209,25 +224,29 @@ class TestCfgToBackend(unittest2.TestCase):
         self.assertEqual(co[0]['name'], "check_tcp")
 
     def test_host_customvariables(self):
-        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete', 'alignak_cfg_files/hosts_custom_variables.cfg'])
-        (stdoutdata, stderrdata) = q.communicate() # now wait
+        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete',
+                              'alignak_cfg_files/hosts_custom_variables.cfg'])
+        q.communicate()  # now wait
 
         result = self.backend.get_all('host')
         hosts = result['_items']
         self.assertEqual(len(hosts), 1)
 
-        print "Found hosts: "
+        print("Found hosts: ")
         for host in hosts:
-            print "- %s, customs: %s" % (host['name'], host['customs'])
+            print("- %s, customs: %s" % (host['name'], host['customs']))
             self.assertEqual(host['customs'], {u'_LOC_LAT': u'45.054700', u'_LOC_LNG': u'5.080856'})
 
 
 class TestContacts(unittest2.TestCase):
-
     @classmethod
     def setUpClass(cls):
+        # Set test mode for alignak backend
+        os.environ['TEST_ALIGNAK_BACKEND'] = '1'
+        os.environ['ALIGNAK_BACKEND_MONGO_DBNAME'] = 'alignak-backend-import-test'
+
         cls.p = subprocess.Popen(['alignak_backend'])
-        print ("Backend PID: %s" % cls.p)
+        print("Backend PID: %s" % cls.p)
         time.sleep(3)
 
         cls.backend = Backend('http://127.0.0.1:5000')
@@ -239,19 +258,20 @@ class TestContacts(unittest2.TestCase):
 
     @classmethod
     def tearDown(cls):
-        print ""
+        print("")
 
     def test_users(self):
-        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete', 'alignak_cfg_files/users.cfg'])
-        (stdoutdata, stderrdata) = q.communicate() # now wait
+        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete',
+                              'alignak_cfg_files/users.cfg'])
+        q.communicate()  # now wait
 
         result = self.backend.get_all('user')
         users = result['_items']
         self.assertEqual(len(users), 4)
 
-        print "Found users: "
+        print("Found users: ")
         for user in users:
-            print "-", user['name']
+            print("-", user['name'])
             if user['name'] == 'admin':
                 self.assertEqual(user['is_admin'], False)
                 self.assertEqual(user['back_role_super_admin'], True)
@@ -260,16 +280,17 @@ class TestContacts(unittest2.TestCase):
                 self.assertEqual(user['back_role_super_admin'], False)
 
     def test_user_is_admin(self):
-        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete', 'alignak_cfg_files/user_admin.cfg'])
-        (stdoutdata, stderrdata) = q.communicate() # now wait
+        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete',
+                              'alignak_cfg_files/user_admin.cfg'])
+        q.communicate()  # now wait
 
         result = self.backend.get_all('user')
         users = result['_items']
         self.assertEqual(len(users), 2)
 
-        print "Found users: "
+        print("Found users: ")
         for user in users:
-            print "-", user['name']
+            print("-", user['name'])
             if user['name'] == 'admin':
                 self.assertEqual(user['is_admin'], False)
                 self.assertEqual(user['back_role_super_admin'], True)
@@ -279,11 +300,14 @@ class TestContacts(unittest2.TestCase):
 
 
 class TestHosts(unittest2.TestCase):
-
     @classmethod
     def setUpClass(cls):
+        # Set test mode for alignak backend
+        os.environ['TEST_ALIGNAK_BACKEND'] = '1'
+        os.environ['ALIGNAK_BACKEND_MONGO_DBNAME'] = 'alignak-backend-import-test'
+
         cls.p = subprocess.Popen(['alignak_backend'])
-        print ("Backend PID: %s" % cls.p)
+        print("Backend PID: %s" % cls.p)
         time.sleep(3)
 
         cls.backend = Backend('http://127.0.0.1:5000')
@@ -295,17 +319,18 @@ class TestHosts(unittest2.TestCase):
 
     @classmethod
     def tearDown(cls):
-        print ""
+        print("")
 
     def test_hosts(self):
 
-        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete', 'alignak_cfg_files/hosts.cfg'])
-        (stdoutdata, stderrdata) = q.communicate() # now wait
+        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete',
+                              'alignak_cfg_files/hosts.cfg'])
+        q.communicate()  # now wait
 
         result = self.backend.get('timeperiod')
         tps = result['_items']
         for tp in tps:
-            print tp['_id'], tp['name']
+            print(tp['_id'], tp['name'])
             if tp['name'] == '24x7':
                 tp_always = tp['_id']
             if tp['name'] == 'Never':
@@ -346,11 +371,11 @@ class TestHosts(unittest2.TestCase):
             self.assertIn('snapshot_period', host)
             self.assertEqual(host['snapshot_period'], tp_never)
 
-
     def test_host_with_double_template(self):
 
-        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete', 'alignak_cfg_files/hosts_2_templates.cfg'])
-        (stdoutdata, stderrdata) = q.communicate() # now wait
+        q = subprocess.Popen(['../alignak_backend_import/cfg_to_backend.py', '--delete',
+                              'alignak_cfg_files/hosts_2_templates.cfg'])
+        q.communicate()  # now wait
 
         r = self.backend.get('host')
         self.assertEqual(len(r['_items']), 1)
@@ -360,4 +385,3 @@ class TestHosts(unittest2.TestCase):
         self.assertEqual(reg_comm['name'], 'srv01')
         self.assertEqual(reg_comm['max_check_attempts'], 6)
         self.assertEqual(reg_comm['check_interval'], 2)
-
--- test/test_import_default.py
+++ test/test_import_default.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest2
+import os
 import time
 import shlex
 import subprocess
@@ -12,6 +13,10 @@ class TestCfgToBackend(unittest2.TestCase):
     @classmethod
     def setUpClass(cls):
         print ("Starting Alignak backend...")
+        # Set test mode for alignak backend
+        os.environ['TEST_ALIGNAK_BACKEND'] = '1'
+        os.environ['ALIGNAK_BACKEND_MONGO_DBNAME'] = 'alignak-backend-import-test'
+
         cls.p = subprocess.Popen(['alignak_backend'])
         print ("Backend PID: %s" % cls.p)
         time.sleep(3)
