6735b873e9c64155e25d1bf08dc023156291afe8
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-10-04 10:28:23 +0200

    Update tests for default configuration

--- test/test_import_default.py
+++ test/test_import_default.py
@@ -12,17 +12,46 @@ class TestCfgToBackend(unittest2.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        print ("Starting Alignak backend...")
-        # Set test mode for alignak backend
+        """
+        This method:
+          * delete mongodb database
+          * start the backend with uwsgi
+
+        :return: None
+        """
+        # Set test mode for Alignak backend
         os.environ['TEST_ALIGNAK_BACKEND'] = '1'
         os.environ['ALIGNAK_BACKEND_MONGO_DBNAME'] = 'alignak-backend-import-test'
 
-        cls.p = subprocess.Popen(['alignak_backend'])
-        print ("Backend PID: %s" % cls.p)
+        # Delete used mongo DBs
+        exit_code = subprocess.call(
+            shlex.split(
+                'mongo %s --eval "db.dropDatabase()"' % os.environ['ALIGNAK_BACKEND_MONGO_DBNAME'])
+        )
+        assert exit_code == 0
+
+        cls.p = subprocess.Popen(['uwsgi', '--plugin', 'python', '-w', 'alignakbackend:app',
+                                  '--socket', '0.0.0.0:5000',
+                                  '--protocol=http', '--enable-threads', '--pidfile',
+                                  '/tmp/uwsgi.pid'])
         time.sleep(3)
+        cls.endpoint = 'http://127.0.0.1:5000'
 
-    def test_import(self):
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Kill uwsgi
 
+        :return: None
+        """
+        subprocess.call(['uwsgi', '--stop', '/tmp/uwsgi.pid'])
+        time.sleep(2)
+
+    def testImport1(self):
+        """
+        Import configuration with backend deletion
+        :return:
+        """
         print ("Feeding backend...")
         exit_code = subprocess.call(
             shlex.split('alignak_backend_import --delete shinken_cfg_files/default/_main.cfg')
