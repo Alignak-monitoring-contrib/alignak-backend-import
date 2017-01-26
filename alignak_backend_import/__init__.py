6e02ea0b94b237dbd8da63b77806530a904deef9
Frédéric MOHIER <frederic.mohier@gmail.com>
2017-01-26 08:25:32 +0100

    Fix pep8 Set version as 0.8.0.1

--- alignak_backend_import/__init__.py
+++ alignak_backend_import/__init__.py
@@ -9,7 +9,7 @@
     an Alignak REST backend.
 """
 # Application version and manifest
-VERSION = (0, 8,0 )
+VERSION = (0, 8, 0, 1)
 
 __application__ = u"Alignak backend import"
 __short_version__ = '.'.join((str(each) for each in VERSION[:2]))
