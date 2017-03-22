19787913521c09a2c6e2cf6d4d309ded33479836
Frédéric MOHIER <frederic.mohier@gmail.com>
2017-03-22 20:19:57 +0100

    Set version as 0.8.6

--- alignak_backend_import/__init__.py
+++ alignak_backend_import/__init__.py
@@ -9,15 +9,15 @@
     an Alignak REST backend.
 """
 # Application version and manifest
-VERSION = (0, 8, 5)
+VERSION = (0, 8, 6)
 
 __application__ = u"Alignak backend import"
 __short_version__ = '.'.join((str(each) for each in VERSION[:2]))
 __version__ = '.'.join((str(each) for each in VERSION[:4]))
 __author__ = u"Frédéric Mohier"
-__author_email__ = u"frederic.mohier@gmail.com"
+__author_email__ = u"frederic.mohier@alignak.net"
 __copyright__ = u"(c) 2015-2016, %s" % __author__
 __license__ = u"GNU Affero General Public License, version 3"
 __description__ = u"Alignak backend import tools"
 __releasenotes__ = u"""Alignak Backend import tools"""
-__doc_url__ = "https://github.com/Alignak-monitoring-contrib/alignak-backend-import"
+__doc_url__ = "http://alignak-backend-import.readthedocs.io/en/develop/?badge=develop"
